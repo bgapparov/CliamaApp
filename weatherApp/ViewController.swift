8bd840a9b495c5cb38c4ddebe8ba0e718cadb8f5
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-07-26 11:58:18 -0500

    First commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Baiaman Gapparov on 7/26/20.
+//  Copyright © 2020 Baiaman Gapparov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
